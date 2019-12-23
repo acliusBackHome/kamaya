6be243c95e939274527bd65b9c0324c93ec549b6
baitieiyi <baitieyi@163.com>
2019-12-23 13:24:47 +0800

    t_测试样例

---
+++ test/t_2.c
@@ -0,0 +1,10 @@
+/*
+ * 测试 变量声明赋值
+ */
+int main() {
+  int test;
+  test = 1;
+  int test1 = 2;
+  long test2 = 3;
+  return 0;
+}
---
+++ test/t_3.c
@@ -0,0 +1,9 @@
+int main() {
+  int i = 0;
+  int output;
+  while(i<10){
+    i = i + 1;
+    output = i;
+  }
+  return 1;
+}
